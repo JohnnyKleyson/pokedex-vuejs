bf1365f112b65be5806a93972a41ea72e6a56a3d
Johnny Kleyson <kleysonjohnny2016@gmail.com>
2020-12-28 04:24:51 -0300

    image

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-      <small>{{url}}</small>
-
+  <div id="pokemon">
         <div class="card">  
             <div class="card-image">
                 <figure>
-                <img :src="pokemon.front" alt="Placeholder image">
+                <img :src="currentImg" alt="Placeholder image" @mouseup="mudarSprit">
                 </figure>
             </div>
             <div class="card-content">
@@ -33,11 +31,14 @@ export default {
             this.pokemon.type = res.data.types[0].type.name;
             this.pokemon.front = res.data.sprites.front_default;
             this.pokemon.back = res.data.sprites.back_default;
-            console.log(this.pokemon)
+            this.currentImg = this.pokemon.front;
+            console.log(this.pokemon);
         })
     },
     data(){
         return {
+            isFront: true,
+            currentImg: '',
             pokemon: {
                 type:  '',
                 front: '',
@@ -55,10 +56,26 @@ export default {
             let newName = value[0] .toUpperCase() + value.slice(1)
             return newName
         }
+    },
+    methods:{
+        mudarSprit: function(){
+            if(this.isFront){
+                this.isFront = false;
+                this.currentImg = this.pokemon.back;
+            }else{
+                this.isFront = true;
+                this.currentImg = this.pokemon.front
+            }
+        }
     }
 }
 </script>
 
 <style>
-
+#pokemon{
+    margin-top: 10px;
+}
+img{
+    cursor: pointer;
+}
 </style>
