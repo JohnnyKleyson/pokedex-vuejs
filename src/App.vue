67f6e9d28058f2e516d33ed27d66709618889cce
Johnny Kleyson <kleysonjohnny2016@gmail.com>
2020-12-28 12:06:30 -0300

    base finalizada

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
     <div class="column is-half is-offset-one-quarter">
-      <div v-for="(poke, index) in pokemons" :key="index">
+      <img src="./assets/pokeshelf.png">
+      <h1 class="title">Poke<span>dex</span></h1>
+      <input class="input is-rounded" type="text" placeholder="Buscar Pokemon Pelo Nome" v-model="busca">
+      <button class="button is-fullwidth is-success" id="buscaBtn" @click="buscar">Buscar</button>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
         <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
       </div>
     </div>
-   
   </div>
 </template>
 
@@ -16,25 +19,46 @@ export default {
   name: 'App',
   data(){
     return{
-      pokemons:[]
+      pokemons:[],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then( res =>{
-      console.log("pegou dados");
       this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
 
     })
   },
   components:{
     Pokemon
-  }
-
+  },
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+       if(this.busca == '' || this.busca == ' ') {
+         this.filteredPokemons = this.pokemons
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
+    }
 }
 </script>
 
 <style>
 #app{
   text-align: center;
+}
+.title{
+  letter-spacing: 1px;
+}
+span{
+
+  color: rgb(238, 38, 38);
+}
+#buscaBtn{
+  margin-top: 10px;
 }
 </style>
